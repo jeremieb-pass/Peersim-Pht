464d1558d57f686f0c0cef2b2398edce17cd3322
Jeremie Baudet <nongbottom@gmail.om>
2016-01-19 00:43:13 +0100

    Exception changes
    Removed one, renamed another.

--- pht/src/peersim/pht/exceptions/CantSplitException.java
+++
@@ -1,7 +0,0 @@
-package peersim.pht.exceptions;
-
-public class CantSplitException extends Exception {
-    public CantSplitException(String message) {
-        super(message);
-    }
-}
--- pht/src/peersim/pht/exceptions/SplitException.java
+++ pht/src/peersim/pht/exceptions/SplitException.java
@@ -1,8 +1,5 @@
 package peersim.pht.exceptions;
 
-/**
- * Exception thrown during a split operation.
- */
 public class SplitException extends PhtException {
     public SplitException(String message) {
         super(message);
