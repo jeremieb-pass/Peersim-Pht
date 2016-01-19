98d256c5a41a3a10dd13be6aad2675e3d9622260
Jeremie Baudet <nongbottom@gmail.om>
2016-01-19 23:07:22 +0100

    Changed name
    Was also used by merge related operation.

--- pht/src/peersim/pht/exceptions/SplitException.java
+++
@@ -1,7 +0,0 @@
-package peersim.pht.exceptions;
-
-public class SplitException extends PhtException {
-    public SplitException(String message) {
-        super(message);
-    }
-}
---
+++ pht/src/peersim/pht/exceptions/SplitMergeException.java
@@ -0,0 +1,7 @@
+package peersim.pht.exceptions;
+
+public class SplitMergeException extends PhtException {
+    public SplitMergeException(String message) {
+        super(message);
+    }
+}
